807e2a0e2bd8ea3f17ed7f130833f6ec6072274c
Muhtasim Fuad Showmik <49075537+Muhtasim-Fuad-Showmik@users.noreply.github.com>
2022-07-03 16:21:46 +0600

    Finished development

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p class="title">
+          &#60; Under Development /&#62;
+        </p>
+        <p>
+          Head back in on 5th June 2022 to explore the fully deployed site.
+        </p>
+        <a
+          className="App-link"
+          href="https://main--peaceful-sable-131357.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Back to Portfolio
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
